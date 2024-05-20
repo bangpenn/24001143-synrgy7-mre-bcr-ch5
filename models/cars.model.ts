98df7cf73f5a2a5e2e2b0a16cfabd2c034812c50
EFENDI <fendikrama56@gmail.com>
2024-05-20 18:55:43 +0700

    feat:fix bugs

--- models/cars.model.ts
+++ models/cars.model.ts
@@ -1,38 +1,42 @@
-import { Model } from 'objection'
-import User from './users.model'
-import Order from './orders.model'
+import { Model } from 'objection';
+import User from './users.model';
+import Order from './orders.model';
 
 class Car extends Model {
-    id!: number
-    user_id!: number
-    name!: string
-    category!: string
-    price!: number
-    start_rent!: Date
-    finish_rent!: Date
-    created_at!: Date
-    updated_at!: Date
+  id!: number;
+  user_id!: number;
+  name!: string;
+  category!: string;
+  price!: number;
+  start_rent!: Date;
+  finish_rent!: Date;
+  created_at!: Date;
+  updated_at!: Date;
 
-    static tableName = 'cars'
+  static get tableName() {
+    return 'cars';
+  }
 
-    static relationMappings = {
-        user: {
-            relation: Model.BelongsToOneRelation,
-            modelClass: User,
-            join: {
-                from: 'cars.user_id',
-                to: 'users.id'
-            }
+  static get relationMappings() {
+    return {
+      owner: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: 'cars.user_id',
+          to: 'users.id',
         },
-        orders: {
-            relation: Model.HasManyRelation,
-            modelClass: Order,
-            join: {
-                from: 'cars.id',
-                to: 'orders.car_id'
-            }
-        }
-    }
+      },
+      orders: {
+        relation: Model.HasManyRelation,
+        modelClass: Order,
+        join: {
+          from: 'cars.id',
+          to: 'orders.car_id',
+        },
+      },
+    };
+  }
 }
 
 export default Car;
--- models/orders.model.ts
+++ models/orders.model.ts
@@ -1,36 +1,40 @@
-import { Model } from 'objection'
-import User from './users.model'
-import Car from './cars.model'
+import { Model } from 'objection';
+import User from './users.model';
+import Car from './cars.model';
 
 class Order extends Model {
-    id!: number;
-    user_id!: number;
-    car_id!: number;
-    start_rent!: Date;
-    finish_rent!: Date;
-    price!: number;
-    status!: string;
-  
-    static tableName = 'orders';
-  
-    static relationMappings = {
+  id!: number;
+  user_id!: number;
+  car_id!: number;
+  start_rent!: Date;
+  finish_rent!: Date;
+  price!: number;
+  status!: string;
+
+  static get tableName() {
+    return 'orders';
+  }
+
+  static get relationMappings() {
+    return {
       user: {
         relation: Model.BelongsToOneRelation,
         modelClass: User,
         join: {
           from: 'orders.user_id',
-          to: 'users.id'
-        }
+          to: 'users.id',
+        },
       },
       car: {
         relation: Model.BelongsToOneRelation,
         modelClass: Car,
         join: {
           from: 'orders.car_id',
-          to: 'cars.id'
-        }
-      }
+          to: 'cars.id',
+        },
+      },
     };
   }
-  
-  export default Order;
+}
+
+export default Order;
